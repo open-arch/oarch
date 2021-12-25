949100f17208a58b3743745188de084cf7815195
gomete <jorgegomezrivas@gmail.com>
2021-12-25 13:52:12 +0100

    feat: add path remove name

--- src/cmd/init.go
+++ src/cmd/init.go
@@ -12,43 +12,24 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var Name string
-
-// initCmd represents the init command
-var initCmd = &cobra.Command{
-	Use:   "init",
-	Short: "Initializes an OpenArch project",
-	Long: "Initializes an OpenArch project with its folder structure and examples.",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		// TODO Handle error?
-		if err := initProject(); err != nil {
-			return err
-		}
-		return nil
-	},
-}
+var (
+	path string
+	initCmd = &cobra.Command{
+		Use:   "init",
+		Short: "Initializes an OpenArch project",
+		Long: "Initializes an OpenArch project with its folder structure and examples.",
+		Run: func(cmd *cobra.Command, args []string) {
+			initProject()
+		},
+	}
+)
 
 func init() {
 	rootCmd.AddCommand(initCmd)
 	
-	initCmd.PersistentFlags().StringVarP(&Name, "name", "n", "", "The name of the project (required)")
-	initCmd.MarkPersistentFlagRequired("name")
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	initCmd.PersistentFlags().StringVar(&path, "path", ".", "The path of the project")
 }
 
-func initProject() error {
-	// TODO Add default name if is null or empty
-
-	fmt.Printf("init called: %v", Name)
-
-	return nil
+func initProject() {
+	fmt.Printf("init called for path: %v", path)
 }
